b5cdd66cc57047c13f504a1b49c9b71305c96fb5
Шурхай Алексей Сергеевич <a.shurkhai@ziko.loc>
2020-10-22 15:45:17 +0300

    feat: add empty input by enter validation

--- momentum/script.js
+++ momentum/script.js
@@ -35,4 +35,59 @@ function addZero(n) {
     return (parseInt(n, 10) < 10 ? '0' : '') + n;
   }
 
+// Get Name
+function getName() {
+  if (localStorage.getItem('name') === null) {
+    name.textContent = '[Enter Name]';
+  } else {
+    name.textContent = localStorage.getItem('name');
+  }
+}
+
+// Set Name
+function setName(e) {
+  if (e.type === 'keypress') {
+    // Make sure enter is pressed
+    if (e.which == 13 && e.target.innerText == "" || e.keyCode == 13 && e.target.innerText == "") {
+      name.innerHTML = '[Type your name]';
+    } else if (e.which == 13 || e.keyCode == 13 && e.target.innerText !== "") {
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem('name', e.target.innerText);
+  }
+}
+
+// Get Focus
+function getFocus() {
+  if (localStorage.getItem('focus') === null) {
+    focus.textContent = '[Enter Focus]';
+  } else {
+    focus.textContent = localStorage.getItem('focus');
+  }
+}
+
+// Set Focus
+function setFocus(e) {
+  if (e.type === 'keypress') {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem('focus', e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem('focus', e.target.innerText);
+  }
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
+
+
+localStorage.clear()
 showTime();
+getName();
+getFocus();
